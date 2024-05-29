25f6c91aa006afefb644f44330a5b5527afd9f11
tteia <tteia.dl@gmail.com>
2024-05-29 15:02:20 +0900

    vi commit this is vi commit

---
+++ basic_syntax/2.basic_commit.sh
@@ -0,0 +1,10 @@
+# 현재 working dirctory, staging area 만! 상태 확인
+git status
+
+# git add
+git add . # 모든 수정 / 추가 사항 add
+git add 특정파일(위치포함) # 특정 파일 add
+git add testfolder/test2.txt
+
+# commit 이력 확인(local repository의 이력)
+git log
