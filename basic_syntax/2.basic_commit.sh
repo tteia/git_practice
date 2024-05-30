f4b0aed2790f2f8c82b19deaf5f14abe7a6171f9
tteia <tteia.dl@gmail.com>
2024-05-30 14:26:38 +0900

    fix

--- basic_syntax/2.basic_commit.sh
+++ basic_syntax/2.basic_commit.sh
@@ -87,5 +87,10 @@ git tag # 태그 목록 조회
 git branch 브랜치명 # 브랜치 생성 / 현재 checkout 하고 있는 브랜치를 기준으로 브랜치 생성됨.
 git checkout 브랜치명 # 해당 브랜치로 전환
 git branch # 브랜치 목록 조회
-
 git branch feature/author_register
+
+git checkout -b 브랜치명 # 브런치 생성과 전환 동시에 ! => 자주 쓰임
+
+# 로컬에서 브랜치 생성 시 주의사항
+# 최신화된 main 에서 checkout -b
+# => git checkout main -> git pull -> git checkout -b feature/test
